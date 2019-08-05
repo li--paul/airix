e81cee6ed2d13f953ff34d9a05b4a87eae1e7f17
Paul Li <llongli@gmail.com>
2019-08-05 16:56:40 +0800

    remove qemu warning

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -fda a.img -hda disk -hdc axfs.img -boot a -curses
+qemu-system-i386 -drive file=a.img,index=0,if=floppy,format=raw -drive file=disk,index=0,media=disk,format=raw -drive file=axfs.img,index=2,media=disk,format=raw -boot a -curses
